c82f45dc349a97a00125bdce6d8ac57ecc04138e
Mitrasish Mukherjee <34746546+rekpero@users.noreply.github.com>
2022-10-08 18:48:25 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,14 @@ npm -v
 yarn -v
 
 
+yum –y install epel-release
+yum repolist
+
 yum –y install vnstat
 vnstat -l
+yum –y install nload
+nload
+yum –y install nethogs
+nethogs
+yum –y install iperf3
+iperf3
